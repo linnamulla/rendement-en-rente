5868a311d09215a215b1513a1013a30cb27879ef
t99forever <ybezema@gmail.com>
2025-02-12 17:00:33 +0100

    Lists toegevoegd aan function voor het berekenen van de waarde van een periodiek bedrag, voor bijhouden data

--- rendement.py
+++ rendement.py
@@ -10,6 +10,8 @@ def vastBedrag(bedragVast, duurJaren, rendementFactor):
 
 # Berekent de verwachte waarde van een periodiek bedrag aan de hand van een looptijd in jaren en een percentage.
 def periodeBedrag(periodeJaren = False, duurJaren = 0, bedragPeriode = 0, rendementFactor = 1):
+    bedragPeriodeTotaal_lijst = []
+    bedragPeriodeWaarde_lijst = []
     if periodeJaren == True:
         while duurJaren >= 0:
             # Berekent de totale oorspronkelijke waarde van een periodiek bedrag.
@@ -18,8 +20,12 @@ def periodeBedrag(periodeJaren = False, duurJaren = 0, bedragPeriode = 0, rendem
             bedragPeriodeWaarde = bedragPeriodeWaarde + bedragPeriode
             rendementPeriode = (bedragPeriodeWaarde * rendementFactor - bedragPeriodeWaarde) / 12
             bedragPeriodeWaarde = bedragPeriodeWaarde + rendementPeriode
+            # Houdt lists bij van het totale bedrag en de totale waarde.
+            bedragPeriodeTotaal_lijst.append(bedragPeriodeTotaal)
+            bedragPeriodeWaarde_lijst.append(bedragPeriodeWaarde)
             # Telt af tot periode afgelopen is.
             duurJaren = duurJaren - 1
+
     else:
         duurMaanden = duurJaren * 12
         while duurMaanden >= 0:
@@ -29,7 +35,10 @@ def periodeBedrag(periodeJaren = False, duurJaren = 0, bedragPeriode = 0, rendem
             bedragPeriodeWaarde = bedragPeriodeWaarde + bedragPeriode
             rendementPeriode = (bedragPeriodeWaarde * rendementFactor - bedragPeriodeWaarde) / 12
             bedragPeriodeWaarde = bedragPeriodeWaarde + rendementPeriode
+            # Houdt lists bij van het totale bedrag en de totale waarde.
+            bedragPeriodeTotaal_lijst.append(bedragPeriodeTotaal)
+            bedragPeriodeWaarde_lijst.append(bedragPeriodeWaarde)
             # Telt af tot periode afgelopen is.
             duurMaanden = duurMaanden - 1
-    return bedragPeriodeTotaal, bedragPeriodeWaarde
+    return bedragPeriodeTotaal, bedragPeriodeWaarde, bedragPeriodeTotaal_lijst, bedragPeriodeWaarde_lijst
 
